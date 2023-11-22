cdd9f6cc40b20a294a33546e26f594488fd9ceb7
Sasan <l.s.arham@gmail.com>
2023-11-22 10:35:46 +0330

    [Add] Add commands

---
+++ Saga/Commands/SagaCommands.cs
@@ -0,0 +1,17 @@
+﻿namespace Saga.Commands
+{
+    public record StartStepOneCommand
+    {
+        public Guid OrderID { get; set; }
+    }
+
+    public record StartStepTwoCommand
+    {
+        public Guid OrderID { get; set; }
+    }
+
+    public record StartStepThreeCommand
+    {
+        public Guid OrderID { get; set; }
+    }
+}
